e9b39ed1a6fd612b9f6f0176f73d1d833a5fd5b6
stumphole145 <nicholas.cheam@gmail.com>
2025-10-08 12:26:33 +0800

    balls

--- main.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-
-// Function to tell stump he sucks at coding
-int main() {
-    printf("Stump sucks at coding!\n");
-    return 0;
-}
