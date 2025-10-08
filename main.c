a64440f475bb6dd88a7cbc7fed554e68b87871f1
sukteMung <tur58332@temple.edu>
2025-10-07 23:59:42 -0400

    Stump sucks

---
+++ main.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+// Function to tell stump he sucks at coding
+int main() {
+    printf("Stump sucks at coding!\n");
+    return 0;
+}
